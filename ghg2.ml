7360f8a8f8104760b18cef32abf56022318bc11d
Mike Kazantsev <mk.fraggod@gmail.com>
2022-01-01 09:35:32 +0500

    ghg2: implement decryption for v2 magic, -p/--pubkey option

--- ghg2.ml
+++ ghg2.ml
@@ -14,9 +14,10 @@
 let cli_conf = ref "/etc/ghg.yaml"
 let cli_enc = ref false
 let cli_dec = ref false
-let cli_key_src = ref ""
-let cli_key_dst = ref []
-let cli_key_dst_add = (fun k -> cli_key_dst := k :: !cli_key_dst)
+let cli_key_sk = ref ""
+let cli_key_pk = ref []
+let cli_key_pk_add = (fun k -> cli_key_pk := !cli_key_pk @ [k])
+let cli_key_convert = ref false
 let cli_files = ref []
 
 let () =
@@ -37,20 +38,23 @@ let () =
 				t^"When decrypting to stdout, authentication/integrity" ^
 				t^" is indicated by exit code (!!!), so ALWAYS CHECK IF EXIT CODE IS 0.\n");
 
-			("-r", Arg.String cli_key_dst_add, " ");
-			("--recipient", Arg.String cli_key_dst_add,
+			("-r", Arg.String cli_key_pk_add, " ");
+			("--recipient", Arg.String cli_key_pk_add,
 				t^"Public key name/id to encrypt to or decrypt with." ^
 				t^"Public key itself can also be specified in pub64-* format." ^
 				t^"Can be specified multiple times to encrypt for each of the keys." ^
 				t^"Private raw64-* keys are not accepted here on purpose," ^
 				t^" to avoid forming a habit of passing these on a command line," ^
 				t^" use pub64-* keys and/or config file instead.");
-			("-k", Arg.Set_string cli_key_src, " ");
-			("--key", Arg.Set_string cli_key_src,
+			("-k", Arg.Set_string cli_key_sk, " ");
+			("--key", Arg.Set_string cli_key_sk,
 				t^"Local secret key name/id to use for encryption ops." ^
 				t^"Same as with -r/--recipient option, raw64-* key strings are not allowed here.\n");
 
-			(* XXX: options for keygen and pubkey conversion *)
+			("-p", Arg.Set cli_key_convert, " ");
+			("--pubkey", Arg.Set cli_key_convert,
+				t^"Print public key for specified -k/--key, or default configured key.\n");
+			(* XXX: -g/--genkey option *)
 
 			("-h", Arg.Set help, " "); ("-help", Arg.Set help, " ") ] in
 	let usage_msg = ("Usage: " ^ Sys.argv.(0) ^ " [opts] [file ...]\
@@ -65,9 +69,12 @@ external nacl_init : unit -> nacl_constants_record = "nacl_init"
 external nacl_key_load : string -> nativeint = "nacl_key_load"
 external nacl_key_free : nativeint -> unit = "nacl_key_free"
 external nacl_key_sk_to_pk : nativeint -> nativeint = "nacl_key_sk_to_pk"
+external nacl_key_b64 : nativeint -> string = "nacl_key_b64"
 external nacl_key_gen : unit -> nativeint = "nacl_key_gen"
 external nacl_key_encrypt : nativeint -> nativeint -> nativeint -> string = "nacl_key_encrypt"
+external nacl_key_decrypt : nativeint -> string -> nativeint = "nacl_key_decrypt"
 external nacl_encrypt : nativeint -> string -> int -> int -> unit = "nacl_encrypt"
+external nacl_decrypt : nativeint -> int -> int -> unit = "nacl_decrypt"
 
 let nacl = nacl_init()
 
@@ -76,12 +83,16 @@ let nacl = nacl_init()
 let magic_v1 = "¯\\_ʻghgʻ_/¯ 1 "
 let magic_v2 = "¯\\_ʻghgʻ_/¯ 2 "
 let magic_len = String.length magic_v1
+let header_end = "---"
+let header_line_len = 256 (* longer lines will raise errors *)
 let fmt = Printf.sprintf
 let fdesc_to_int : Unix.file_descr -> int = Obj.magic (* ocamllabs/ocaml-ctypes#123 *)
 let rec eintr_loop f x =
 	try f x with Unix.Unix_error (Unix.EINTR, _, _) -> eintr_loop f x
-exception KeyParseFail of string
 
+
+exception KeyParseFail of string
+exception DecryptFail of string
 
 let parse_key =
 	let re_key_pub64, re_key_raw64 =
@@ -96,56 +107,90 @@ let parse_key =
 				then parse_key_b64 (Str.matched_group 1 spec) else 0n in
 			let sk = if Str.string_match re_key_raw64 spec 0
 				then parse_key_b64 (Str.matched_group 1 spec) else 0n in
-			if pk == 0n && sk == 0n then raise (KeyParseFail "unknown key format");
-			if is_sk && sk == 0n then raise (KeyParseFail "secret key is required");
-			if is_sk then sk else if pk != 0n then pk else nacl_key_sk_to_pk sk
+			if pk = 0n && sk = 0n then raise (KeyParseFail "unknown key format");
+			if is_sk && sk = 0n then raise (KeyParseFail "secret key is required");
+			if is_sk then sk else if pk <> 0n then pk else nacl_key_sk_to_pk sk
 		with KeyParseFail err -> raise (KeyParseFail (fmt "%s (%s)" err key_name))
 
-let encrypt header fdesc_src fdesc_dst =
-	let key = nacl_key_gen () in
-	let key_ptrs = ref [] in
-	let key_encs = Fun.protect
-		~finally:(fun () -> List.iter (fun k -> nacl_key_free k) !key_ptrs)
-		( fun () ->
-			key_ptrs := [parse_key "-k/--key" true !cli_key_src];
-			let m = List.length !cli_key_dst in (* cli_key_dst is reversed, hence m-n *)
-			List.iteri ( fun n k -> key_ptrs :=
-				parse_key (fmt "-r/--recipient #%d" (m-n)) false k :: !key_ptrs ) !cli_key_dst;
-			let sk, pk_list = List.(hd !key_ptrs, tl !key_ptrs) in
-			List.map (fun pk -> nacl_key_encrypt sk pk key) pk_list ) in
 
-	let buff = Bytes.create 128 in (* should be longer than magic/key_enc *)
+let encrypt key key_encs chunk0 fdesc_src fdesc_dst =
+	let buff = Bytes.create header_line_len in
 	let write_line s =
 		let s_len = String.length s in
 		Bytes.blit_string s 0 buff 0 s_len; Bytes.set buff s_len '\n';
 		let buff_len = s_len + 1 in
 		if (Unix.write fdesc_dst buff 0 buff_len) < buff_len
 			then raise (Failure "Write to destination file/fd failed") in
-
 	write_line (magic_v2 ^ "-");
 	List.iter write_line key_encs;
-	write_line "---";
-	nacl_encrypt key header (fdesc_to_int fdesc_src) (fdesc_to_int fdesc_dst)
+	write_line header_end;
+	nacl_encrypt key chunk0 (fdesc_to_int fdesc_src) (fdesc_to_int fdesc_dst)
+
+
+let decrypt sk fdesc_src fdesc_dst =
+	let line_buff = Bytes.create header_line_len in
+	let rec read_line n =
+		let m = try eintr_loop (Unix.read fdesc_src line_buff n) 1
+			with Invalid_argument err -> raise (DecryptFail (fmt "too long key line [pos=%d]" n)) in
+		if m = 0 || (Bytes.get line_buff n) = '\n'
+			then Bytes.sub_string line_buff 0 n else read_line (n + 1) in
+	let rec read_key sk key =
+		let key_enc_b64 = read_line 0 in
+		if key_enc_b64 = header_end then key else
+		let key = if key <> 0n then key else
+			try nacl_key_decrypt sk key_enc_b64
+			with Failure err ->
+				if err = "crypto_box_open_easy failed" then key else raise (Failure err) in
+		read_key sk key in
+	let _magic_tail = read_line 0 in
+	let key = match read_key sk 0n with
+		| 0n -> raise (DecryptFail "key mismatch") | key -> key in
+	nacl_decrypt key (fdesc_to_int fdesc_src) (fdesc_to_int fdesc_dst)
 
 
 let () =
 	(* XXX: implement src/dst file handling *)
 	(* XXX: use keys from config file *)
-	(* XXX: bark at raw64- keys in pk_list, when config will be implemented *)
-	if !cli_key_src == "" then ( Printf.eprintf
+	if !cli_key_sk = "" then ( prerr_endline (* XXX *)
 		"ERROR: -k/--key option is required, as -c/--conf is not implemented"; exit 1 );
-	if (List.length !cli_key_dst) == 0 then ( Printf.eprintf
-		"ERROR: -r/--recipient option is required, as -c/--conf is not implemented"; exit 1 );
 
-	let magic = Bytes.make magic_len '\x00' in
+	if !cli_key_convert then
+		let pk = parse_key "-k/--key" false !cli_key_sk in
+		Fun.protect
+			~finally:(fun () -> nacl_key_free pk)
+			(fun () -> print_endline ("pub64-" ^ (nacl_key_b64 pk)); exit 0)
+	else
+
+	let magic = Bytes.create magic_len in
 	let rec magic_read n =
 		if n >= magic_len then Bytes.to_string magic else
 		match eintr_loop (Unix.read Unix.stdin magic n) (magic_len - n) with
 		| 0 -> Bytes.sub_string magic 0 n
 		| m -> magic_read (n + m) in
 	match magic_read 0 with
+
 	| s when s = magic_v1 -> raise (Failure "magic_v1 decrypt")
 		(* for box_dst_pk in box_dst_pk_list: *)
 		(* readline -> src_pk, nonce_16B, dst_pkid_8B *)
-	| s when s = magic_v2 -> raise (Failure "magic_v2 decrypt")
-	| s -> encrypt s Unix.stdin Unix.stdout
+
+	| s when s = magic_v2 ->
+		let sk = parse_key "-k/--key" true !cli_key_sk in
+		Fun.protect
+			~finally:(fun () -> nacl_key_free sk)
+			(fun () -> decrypt sk Unix.stdin Unix.stdout)
+
+	| s ->
+		(* XXX: bark at raw64- keys in pk_list when config will be implemented *)
+		if (List.length !cli_key_pk) = 0 then ( prerr_endline (* XXX *)
+			"ERROR: -r/--recipient option is required, as -c/--conf is not implemented"; exit 1 );
+		let key = nacl_key_gen () in
+		let pk_list = ref [] in
+		Fun.protect
+			~finally:(fun () -> List.iter (fun k -> nacl_key_free k) !pk_list)
+			( fun () ->
+				pk_list := [parse_key "-k/--key" true !cli_key_sk];
+				List.iteri ( fun n k -> pk_list :=
+					!pk_list @ [parse_key (fmt "-r/--recipient #%d" (n + 1)) false k] ) !cli_key_pk;
+				let sk, pk_list = List.(hd !pk_list, tl !pk_list) in
+				let key_encs = List.map (fun pk -> nacl_key_encrypt sk pk key) pk_list in
+				encrypt key key_encs s Unix.stdin Unix.stdout )
--- ghg2.ml.c
+++ ghg2.ml.c
@@ -15,19 +15,19 @@
 #include <sodium.h>
 
 
-int key_bs;
-int b64_type = sodium_base64_VARIANT_URLSAFE_NO_PADDING;
-int enc_bs = 16384;
+const int enc_bs = 16384; // note: changing requires protocol version bump
+const int key_bs = crypto_secretstream_xchacha20poly1305_KEYBYTES;
+const int b64_type = sodium_base64_VARIANT_URLSAFE_NO_PADDING;
+const int nonce_len = crypto_box_NONCEBYTES;
+const int ct_len = key_bs + crypto_box_MACBYTES;
 
 value nacl_init() {
 	CAMLparam0();
 	CAMLlocal1(v_consts);
 	v_consts = caml_alloc_tuple(2);
-
 	if (sodium_init()) caml_failwith("sodium_init failed");
 
 	// Same lengths are used for all keys when parsing, so checked to be same here
-	key_bs = crypto_secretstream_xchacha20poly1305_KEYBYTES;
 	if ( key_bs != crypto_box_PUBLICKEYBYTES || key_bs != crypto_box_SECRETKEYBYTES)
 		caml_failwith("unexpected libsodium key length values");
 
@@ -43,16 +43,16 @@ value nacl_key_load(value v_key_b64) {
 	size_t key_b64_len = caml_string_length(v_key_b64);
 	unsigned char *key_b64 = String_val(v_key_b64);
 	const char *key_b64_end;
-	size_t key_bin_len;
-	unsigned char *key_bin = sodium_malloc(key_bs);
-	if (sodium_base642bin( key_bin, key_bs,
-			key_b64, key_b64_len, NULL, &key_bin_len, &key_b64_end, b64_type ))
+	size_t key_len;
+	unsigned char *key = sodium_malloc(key_bs);
+	if (sodium_base642bin( key, key_bs,
+			key_b64, key_b64_len, NULL, &key_len, &key_b64_end, b64_type ))
 		{ err = "sodium_base642bin failed"; goto fail_with_err; }
-	if (key_bin_len != key_bs) { err = "key length mismatch"; goto fail_with_err; }
-	CAMLreturn(caml_copy_nativeint((intptr_t) key_bin));
+	if (key_len != key_bs) { err = "key length mismatch"; goto fail_with_err; }
+	CAMLreturn(caml_copy_nativeint((intptr_t) key));
 
 	fail_with_err:
-	sodium_free(key_bin);
+	sodium_free(key);
 	caml_failwith(err);
 }
 
@@ -62,13 +62,22 @@ value nacl_key_free(value v_key) {
 	CAMLreturn(Val_unit);
 }
 
-value nacl_key_sk_to_pk(value v_key_sk) {
-	CAMLparam1(v_key_sk);
-	unsigned char *key_sk = (unsigned char *) Nativeint_val(v_key_sk);
+value nacl_key_sk_to_pk(value v_sk) {
+	CAMLparam1(v_sk);
+	unsigned char *key_sk = (unsigned char *) Nativeint_val(v_sk);
 	unsigned char *key_pk = sodium_malloc(key_bs);
 	if (crypto_scalarmult_base(key_pk, key_sk))
 		{ sodium_free(key_pk); caml_failwith("crypto_scalarmult_base failed"); }
 	CAMLreturn(caml_copy_nativeint((intptr_t) key_pk));
+}
+
+value nacl_key_b64(value v_key) {
+	CAMLparam1(v_key);
+	unsigned char *key = (unsigned char *) Nativeint_val(v_key);
+	int key_b64_len = sodium_base64_encoded_len(key_bs, b64_type);
+	unsigned char key_b64[key_b64_len];
+	sodium_bin2base64(key_b64, key_b64_len, key, key_bs, b64_type);
+	CAMLreturn(caml_alloc_initialized_string(key_b64_len-1, key_b64));
 }
 
 value nacl_key_gen() {
@@ -83,25 +92,52 @@ value nacl_key_encrypt(value v_sk, value v_pk, value v_key) {
 	unsigned char *key_sk = (unsigned char *) Nativeint_val(v_sk);
 	unsigned char *key_pk = (unsigned char *) Nativeint_val(v_pk);
 	unsigned char *key = (unsigned char *) Nativeint_val(v_key);
+	int nct_len = key_bs + nonce_len + ct_len;
+	unsigned char nct[nct_len];
+	memcpy(nct, key_pk, key_bs);
+	randombytes_buf(nct + key_bs, nonce_len);
 
-	int nonce_len = crypto_box_NONCEBYTES;
-	int ct_len = key_bs + crypto_box_MACBYTES;
-	unsigned char ct[nonce_len + ct_len];
-	randombytes_buf(ct, nonce_len);
-	if (crypto_box_easy(ct + nonce_len, key, key_bs, ct, key_pk, key_sk))
+	if (crypto_box_easy( nct + key_bs + nonce_len,
+			key, key_bs, nct + key_bs, key_pk, key_sk ))
 		caml_failwith("crypto_box_easy failed");
 
-	int ct_b64_len = sodium_base64_encoded_len(nonce_len + ct_len, b64_type);
+	int ct_b64_len = sodium_base64_encoded_len(nct_len, b64_type);
 	unsigned char ct_b64[ct_b64_len];
-	sodium_bin2base64(ct_b64, ct_b64_len, ct, nonce_len + ct_len, b64_type);
-
+	sodium_bin2base64(ct_b64, ct_b64_len, nct, nct_len, b64_type);
 	CAMLreturn(caml_alloc_initialized_string(ct_b64_len-1, ct_b64));
 }
 
-value nacl_encrypt(value v_key, value v_chunk, value v_fd_src, value v_fd_dst) {
-	CAMLparam4(v_key, v_chunk, v_fd_src, v_fd_dst);
+value nacl_key_decrypt(value v_sk, value v_ct_b64) {
+	CAMLparam2(v_sk, v_ct_b64);
+	unsigned char *key_sk = (unsigned char *) Nativeint_val(v_sk);
+	unsigned char *ct_b64 = String_val(v_ct_b64);
+	size_t ct_b64_len = caml_string_length(v_ct_b64);
 	char *err;
-	unsigned char *chunk, *key = (unsigned char *) Nativeint_val(v_key);
+	unsigned char *key = sodium_malloc(key_bs);
+	const char *key_b64_end;
+	size_t nct_len;
+	unsigned char nct[key_bs + nonce_len + ct_len];
+
+	if (sodium_base642bin( nct, key_bs + nonce_len + ct_len,
+			ct_b64, ct_b64_len, NULL, &nct_len, &key_b64_end, b64_type ))
+		{ err = "sodium_base642bin failed"; goto fail_with_err; }
+	if (nct_len != key_bs + nonce_len + ct_len)
+		{ err = "key length mismatch"; goto fail_with_err; }
+
+	if (crypto_box_open_easy( key,
+			nct + key_bs + nonce_len, ct_len, nct + key_bs, nct, key_sk ))
+		{ err = "crypto_box_open_easy failed"; goto fail_with_err; }
+	CAMLreturn(caml_copy_nativeint((intptr_t) key));
+
+	fail_with_err:
+	sodium_free(key);
+	caml_failwith(err);
+}
+
+value nacl_encrypt(value v_key, value v_chunk0, value v_fd_src, value v_fd_dst) {
+	CAMLparam4(v_key, v_chunk0, v_fd_src, v_fd_dst);
+	char *err;
+	unsigned char *key = (unsigned char *) Nativeint_val(v_key);
 	int fd_src = -1, fd_dst = -1; FILE *fp_src, *fp_dst;
 
 	unsigned char buff_in[enc_bs];
@@ -111,36 +147,85 @@ value nacl_encrypt(value v_key, value v_chunk, value v_fd_src, value v_fd_dst) {
 	crypto_secretstream_xchacha20poly1305_state st;
 	unsigned long long out_len; size_t in_len; int eof; unsigned char tag;
 
+	in_len = caml_string_length(v_chunk0);
+	if (in_len >= enc_bs) { err = "enc bug - initial chunk too large"; goto fail_with_err; }
+	if (in_len > 0) { memcpy(buff_in, String_val(v_chunk0), in_len); }
+
 	fd_src = dup(Int_val(v_fd_src)); fd_dst = dup(Int_val(v_fd_dst));
 	if (fd_src < 0 || fd_dst < 0) { err = "enc fd dup failed"; goto fail_with_err; }
-	fp_src = fdopen(Int_val(v_fd_src), "r"); fp_dst = fdopen(Int_val(v_fd_dst), "w");
+	fp_src = fdopen(Int_val(v_fd_src), "rb"); fp_dst = fdopen(Int_val(v_fd_dst), "wb");
 	if (!fp_src || !fp_dst) { err = "enc fdopen failed"; goto fail_with_err; }
 
 	if (crypto_secretstream_xchacha20poly1305_init_push(&st, hdr, key))
 		{ err = "enc init_push failed"; goto fail_with_err; }
-	fwrite(hdr, 1, hdr_len, fp_dst);
-	if (feof(fp_dst) || ferror(fp_dst)) { err = "enc header write failed"; goto fail_with_err; }
-
-	in_len = caml_string_length(v_chunk);
-	if (in_len > enc_bs) { err = "enc bug - initial chunk too large"; goto fail_with_err; }
-	if (in_len > 0) chunk = String_val(v_chunk);
+	if (fwrite(hdr, 1, hdr_len, fp_dst) != hdr_len)
+		{ err = "enc header write failed"; goto fail_with_err; }
 
 	do {
-		if (!chunk) {
-			in_len = fread(buff_in, 1, enc_bs, fp_src);
-			eof = feof(fp_src); }
-		else {
-			memcpy(buff_in, chunk, in_len);
-			chunk = NULL; }
+		in_len += fread(buff_in + in_len, 1, sizeof buff_in - in_len, fp_src);
+		eof = feof(fp_src);
 		tag = eof ? crypto_secretstream_xchacha20poly1305_TAG_FINAL : 0;
 		if (crypto_secretstream_xchacha20poly1305_push(
 				&st, buff_out, &out_len, buff_in, in_len, NULL, 0, tag ))
 			{ err = "enc push failed"; goto fail_with_err; }
-		fwrite(buff_out, 1, (size_t) out_len, fp_dst);
-		if (ferror(fp_src) || feof(fp_dst) || ferror(fp_dst)) { err = "enc i/o error"; goto fail_with_err; }
+		if (fwrite(buff_out, 1, (size_t) out_len, fp_dst) != out_len || ferror(fp_src))
+			{ err = "enc i/o error"; goto fail_with_err; }
+		in_len = 0;
 	} while (!eof);
 
-	fclose(fp_src); fclose(fp_dst);
+	int close_err;
+	if ( (close_err = fflush(fp_dst))
+			|| ((close_err |= fclose(fp_src)) & 0 || (fp_src = NULL)) || close_err
+			|| ((close_err |= fclose(fp_dst)) & 0 || (fp_dst = NULL)) || close_err )
+		{ err = "enc i/o flush/close error"; goto fail_with_err; }
+	CAMLreturn(Val_unit);
+
+	fail_with_err:
+	if (fp_src) fclose(fp_src); else if (fd_src >= 0) close(fd_src);
+	if (fp_dst) fclose(fp_dst); else if (fd_dst >= 0) close(fd_dst);
+	caml_failwith(err);
+}
+
+value nacl_decrypt(value v_key, value v_fd_src, value v_fd_dst) {
+	CAMLparam3(v_key, v_fd_src, v_fd_dst);
+	char *err;
+	unsigned char *key = (unsigned char *) Nativeint_val(v_key);
+	int fd_src = -1, fd_dst = -1; FILE *fp_src, *fp_dst;
+
+	unsigned char buff_in[enc_bs + crypto_secretstream_xchacha20poly1305_ABYTES];
+	unsigned char buff_out[enc_bs];
+	size_t hdr_len = crypto_secretstream_xchacha20poly1305_HEADERBYTES;
+	unsigned char hdr[hdr_len];
+	crypto_secretstream_xchacha20poly1305_state st;
+	unsigned long long out_len; size_t in_len; int eof; unsigned char tag;
+
+	fd_src = dup(Int_val(v_fd_src)); fd_dst = dup(Int_val(v_fd_dst));
+	if (fd_src < 0 || fd_dst < 0) { err = "dec fd dup failed"; goto fail_with_err; }
+	fp_src = fdopen(Int_val(v_fd_src), "rb"); fp_dst = fdopen(Int_val(v_fd_dst), "wb");
+	if (!fp_src || !fp_dst) { err = "dec fdopen failed"; goto fail_with_err; }
+
+	if (fread(hdr, 1, hdr_len, fp_src) != hdr_len)
+		{ err = "dec header read failed"; goto fail_with_err; }
+	if (crypto_secretstream_xchacha20poly1305_init_pull(&st, hdr, key))
+		{ err = "dec init_pull failed"; goto fail_with_err; }
+
+	do {
+		in_len = fread(buff_in, 1, sizeof buff_in, fp_src);
+		eof = feof(fp_src);
+		if (crypto_secretstream_xchacha20poly1305_pull(
+				&st, buff_out, &out_len, &tag, buff_in, in_len, NULL, 0 ))
+			{ err = "dec pull failed"; goto fail_with_err; }
+		if (tag == crypto_secretstream_xchacha20poly1305_TAG_FINAL && !eof)
+			{ err = "dec incomplete stream"; goto fail_with_err; }
+		if (fwrite(buff_out, 1, (size_t) out_len, fp_dst) != out_len || ferror(fp_src))
+			{ err = "dec i/o error"; goto fail_with_err; }
+	} while (!eof);
+
+	int close_err;
+	if ( (close_err = fflush(fp_dst))
+			|| ((close_err |= fclose(fp_src)) & 0 && (fp_src = NULL)) || close_err
+			|| ((close_err |= fclose(fp_dst)) & 0 && (fp_dst = NULL)) || close_err )
+		{ err = "dec i/o flush/close error"; goto fail_with_err; }
 	CAMLreturn(Val_unit);
 
 	fail_with_err:
